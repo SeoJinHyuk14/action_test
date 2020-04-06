5c14dcf28d259466c8b78c97502ad2d3fe2106da
KaSha(서진혁) <kasha.seo@kakaocorp.com>
2020-04-06 20:37:43 +0900

    jacoco 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.3.71"
 	kotlin("plugin.spring") version "1.3.71"
 	id("io.gitlab.arturbosch.detekt").version("1.7.4")
+	id("jacoco")
 }
 
 group = "com.kasha"
@@ -69,4 +70,44 @@ detekt {
 //			destination = file("build/reports/detekt.json") // Path where report will be stored
 //		}
 	}
+}
+
+jacoco {
+	// JaCoCo 버전
+	toolVersion = "0.8.5"
+
+//  테스트결과 리포트를 저장할 경로 변경
+//  default는 "${project.reporting.baseDir}/jacoco"
+//  reportsDir = file("$buildDir/customJacocoReportDir")
+}
+
+tasks.jacocoTestReport {
+	reports {
+		// 원하는 리포트를 켜고 끌 수 있습니다.
+		html.isEnabled = true
+		xml.isEnabled = false
+		csv.isEnabled = false
+
+//  각 리포트 타입 마다 리포트 저장 경로를 설정할 수 있습니다.
+  html.destination = file("$buildDir/jacocoHtml")
+  xml.destination = file("$buildDir/jacoco.xml")
+	}
+}
+
+tasks.jacocoTestCoverageVerification {
+	violationRules {
+		rule {
+			element = "CLASS"
+
+			limit {
+				counter = "BRANCH"
+				value = "COVEREDRATIO"
+				minimum = "0.30".toBigDecimal()	// 최소 커버리지 조절
+			}
+		}
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
---
+++ src/test/kotlin/hello/KotlinFooTest.kt
@@ -0,0 +1,16 @@
+package hello
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class KotlinFooTest {
+
+	private val kotlinFoo = KotlinFoo()
+
+	@Test
+	fun `partially covered hello method test`() {
+		val actual = kotlinFoo.hello("펭")
+		assertEquals(actual, "하")
+	}
+
+}
